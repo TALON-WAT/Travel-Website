7c5bac4c955333595604f06d6593dd3ee0bf3dff
TALON-WAT <talonwat@gmail.com>
2019-10-21 11:16:27 +0800

    1.1 登录功能

---
+++ src/main/java/web/servlet/RegisterServlet.java
@@ -0,0 +1,23 @@
+package web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author TALON WAT
+ * @date 2019-10-21 11:15
+ */
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
